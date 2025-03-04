05ee528aa6eb9effcdfa08d83ae693610e720b25
ISDelchilov21 <ISDelchilov21@codingburgas.bg>
2025-03-04 10:12:06 +0200

    Fix deployment pt. 3.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,9 +12,7 @@ const config: Config = {
       },
     },
     extend: {
-      fontFamily: {
-        sansation: ['"Sansation Light"', 'sans-serif'],
-      },
+
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
